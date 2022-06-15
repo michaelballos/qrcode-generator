3c8a5a909d9a8a267c303c5e428e162cf1ba9c4f
michaelballos <mdb3091207@gmai.com>
2022-06-14 20:28:22 -0500

    Add qrcode generators

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, {useState, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios, { AxiosResponse } from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [qrcode, setQrcode] = useState();
+    const [simulatedQrcode, setSimulatedQrcode] = useState(false);
+
+    const onClick = useCallback(() => {
+      axios.get('http://localhost:4000/qrcode').then((response: AxiosResponse)=> {
+        setQrcode(response.data);
+      })
+    }, [qrcode]);
+
+
+    const simulatedFetch = useCallback(() => {
+        setSimulatedQrcode(!simulatedQrcode);
+    }, [simulatedQrcode])
+
+    return (
+        <div className="App">
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    gap: '20px',
+                    width: '100%',
+                }}
+            >
+                {
+                    !simulatedQrcode
+                        ? <img src={logo} className="App-logo" alt="logo"/>
+                        : <img
+                            style={{
+                            }}
+                            src='https://upload.wikimedia.org/wikipedia/commons/d/d0/QR_code_for_mobile_English_Wikipedia.svg'
+                            alt='code'
+                        />
+                }
+                {
+                    qrcode === undefined
+                        ? <img src={logo} className="App-logo" alt="logo" />
+                        : <img src ={`data:image/png;base64,${qrcode}`} alt="qrcode" />
+                }
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    gap: '400px',
+                    width: '100%',
+                }}
+            >
+                <button
+                    onClick={simulatedFetch}
+                >
+                    Simulate Generation
+                </button>
+                <button
+                    onClick={onClick}
+                >
+                    Generate Code
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
